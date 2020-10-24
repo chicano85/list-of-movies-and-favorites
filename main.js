dfaf12c0c2c2e3765526cdefc8affc4da296fb4e
chicano85 <chicano.cano@gmail.com>
2020-10-24 12:44:44 +0200

    add paintFavorites

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const sectionSearch = document.querySelector(".js-sectionSearch");
-
 const listShows = document.querySelector(".js-listShows");
 
 const btnSearch = document.querySelector(".js-buttonSearch");
@@ -9,13 +7,14 @@ const btnSearch = document.querySelector(".js-buttonSearch");
 const inputSearch = document.querySelector(".js-inputSearch");
 
 let shows = [];
-let favourites = [];
+let favorites = [];
 
 // Conectar con la API
 
 function getData() {
   const nameShow = inputSearch.value;
-  fetch(`https://api.tvmaze.com/search/shows?q=${nameShow}`)
+  const apiUrl = "https://api.tvmaze.com/search/shows?q=";
+  fetch(`${apiUrl} + ${nameShow}`)
     .then(function (response) {
       return response.json();
     })
@@ -23,8 +22,13 @@ function getData() {
       shows = data;
       paintShows();
       listenShows();
+      paintFavorites();
     });
 }
+
+btnSearch.addEventListener("click", getData);
+
+getData();
 
 // Pintar las series en el HTML
 
@@ -32,47 +36,71 @@ function paintShows() {
   let showContent = "";
   showContent = "<ul>";
   for (let i = 0; i < shows.length; i++) {
-    showContent += `<li class="js-itemShow" id="${[shows[i].show.id]}">`;
-    showContent += `<h2>${shows[i].show.name}</h2>`;
-
-    showContent += `<img src="${shows[i].show.image.medium}" alt="image show" title="image show" />`;
-    showContent += "</li>";
-
-    let image;
-    if (shows.image === null) {
-      image = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+    showContent += `<li class="js-itemShow" id="${i}">`;
+    if (shows[i].show.image != null) {
+      let image = shows[i].show.image.medium;
+      showContent += `<img src="${image}" alt="image show" title="image show" />`;
     } else {
-      image = shows[i].show.image.medium;
+      image = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+      showContent += `<img src="${image}" alt="image show" title="image show" />`;
     }
+    showContent += `<h2>${shows[i].show.name}</h2>`;
+    showContent += "</li>";
   }
   showContent += "</ul>";
   listShows.innerHTML += showContent;
 }
 
-// Función que marca los favoritos
+// Pintar favoritos
 
-function favoritesShows(ev) {
-  console.log("escucho evento");
-  const clicked = ev.currentTarget.id;
-  console.log(clicked);
+const favList = document.querySelector(".js-listFavorites");
+
+function paintFavorites() {
+  console.log("Hola, favoritos");
+
+  favContent = "";
+  favContent = "<ul>";
+
+  for (let i = 0; i < favorites.lengt; i++) {
+    favContent += `<li class="js-favItem" id="${i}">`;
+    if (favorites[i].show.image != null) {
+      let imageFav = shows[i].show.image.medium;
+      favContent += `<img src="${imageFav}" alt="image show" title="image show" />`;
+    } else {
+      imageFav = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+      favContent += `<img src="${image}" alt="image show" title="image show" />`;
+    }
+    favContent += `<h2>${shows[i].show.name}</h2>`;
+    favContent += "</li>";
+  }
+  favContent += "</ul>";
+  favList.innerHTML += favContent;
+}
+
+// Añadir favoritos
+
+function addFavorites(ev) {
+  const clicked = parseInt(ev.currentTarget.id);
+  const indexFav = favorites.indexOf(clicked);
+  const isFavorite = indexFav !== -1;
+  console.log(isFavorite);
+
+  if (isFavorite === false) {
+    favorites.push(clicked);
+    clicked.classList.add("favoriteColor");
+  } else {
+    favorites.splice(indexFav, 1);
+    clicked.classList.remove("favoriteColor");
+  }
+
+  listenShows();
 }
 
 // Función que escucha el evento
 
 function listenShows() {
-  console.log("listen");
   const itemsShow = document.querySelectorAll(".js-itemShow");
   for (const itemShow of itemsShow) {
-    console.log("entrando en for");
-    itemShow.addEventListener("click", favoritesShows);
+    itemShow.addEventListener("click", addFavorites);
   }
 }
-
-// Buscar las series
-
-function searchShows() {
-  console.log("buscar serie");
-  getData();
-}
-
-btnSearch.addEventListener("click", searchShows);
